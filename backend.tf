a9e9b186de69037d3d13330b0adf65e7c9ac3956
freeboundarylab <freeboundarylab@gmail.com>
2023-10-19 00:17:34 +0200

    key

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "freeboundarylab-lulgabc-learning"
-    key            = "state/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "eu-west-3"
     dynamodb_table = "terraform"
     encrypt        = true
